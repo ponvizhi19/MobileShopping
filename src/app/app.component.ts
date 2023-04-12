a6aa7413a55520779cd7cc648427b98e2adba726
ponvizhi19 <ponvizhi.2k@gmail.com>
2023-04-12 14:34:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MobileShopping';
+  name = 'Ponvizhi Uday';
+  Mobilenumber = 9094109443;
+  age = 22;
+  company = 'aspire systems';
+  position='center';
+  message = 'Good Morning';
+  changeMessage(){
+    this.message="Greetings!!"
+  }
 }
